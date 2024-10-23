c48f46088aa484f9fb21ea9f28026ffb2480da74
BenAtWHOI <benjamin.foley@whoi.edu>
2024-10-23 11:47:38 -0400

    Added client and test methods for store, s3cfg, and identifier

--- apiclient.py
+++ apiclient.py
@@ -1,4 +1,5 @@
 import requests
+from utils.custom_exception import BadRequestException, ClientError, NonRetryableError, RetryableError
 
 class ApiClient:
     def __init__(self, base_url: str, username: str, password: str):
@@ -93,3 +94,331 @@ class ApiClient:
         else:
             raise Exception(f"Failed to list media. Status code: {response.status_code}  Response: {response.content}")
 
+    def list_stores(self):
+        """
+        List all stores.
+        """
+        url = f"{self.base_url}/api/stores"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to list stores. Status code: {response.status_code}  Response: {response.content}")
+
+    def get_store(self, store_id):
+        """
+        Get store by id.
+        """
+        url = f"{self.base_url}/api/store/{store_id}"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to get store (id {store_id}). Status code: {response.status_code}  Response: {response.content}")
+
+    def create_store(self, create_params: dict):
+        """
+        Create a store.
+        """
+        url = f"{self.base_url}/api/store"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.post(url, headers=self.headers, json=create_params)
+
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 422:
+            raise BadRequestException(f"Invalid request: {create_params}. Status code: {response.status_code}  Response: {response.content}")
+        else:
+            raise Exception(f"Failed to create store. Status code: {response.status_code}  Response: {response.content}")
+
+    def delete_store(self, store_id):
+        """
+        Delete a store.
+        """
+        url = f"{self.base_url}/api/store/{store_id}"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.delete(url, headers=self.headers)
+
+        if response.status_code == 204:
+            return
+        else:
+            raise Exception(f"Failed to delete store. Status code: {response.status_code}  Response: {response.content}")
+
+    def list_s3cfgs(self):
+        """
+        List all s3cfgs.
+        """
+        url = f"{self.base_url}/api/s3cfgs"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to list s3cfgs. Status code: {response.status_code}  Response: {response.content}")
+
+    def get_s3cfg(self, s3cfg_id):
+        """
+        Get s3cfg by id.
+        """
+        url = f"{self.base_url}/api/s3cfg/{s3cfg_id}"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to get s3cfg (id {s3cfg_id}). Status code: {response.status_code}  Response: {response.content}")
+
+    def create_s3cfg(self, create_params: dict):
+        """
+        Create a s3cfg.
+        """
+        url = f"{self.base_url}/api/s3cfg"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.post(url, headers=self.headers, json=create_params)
+
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 422:
+            raise BadRequestException(f"Invalid request: {create_params}. Status code: {response.status_code}  Response: {response.content}")
+        else:
+            raise Exception(f"Failed to create s3cfg. Status code: {response.status_code}  Response: {response.content}")
+
+    def update_s3cfg(self, s3cfg_id, update_params: dict):
+        """
+        Update a s3cfg.
+        """
+        url = f"{self.base_url}/api/s3cfg/{s3cfg_id}"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.put(url, headers=self.headers, json=update_params)
+
+        if response.status_code == 204:
+            return
+        elif response.status_code == 422:
+            raise BadRequestException(f"Invalid request: {update_params}. Status code: {response.status_code}  Response: {response.content}")
+        else:
+            raise Exception(f"Failed to update s3cfg. Status code: {response.status_code}  Response: {response.content}")
+
+    def delete_s3cfg(self, s3cfg_id):
+        """
+        Delete a s3cfg.
+        """
+        url = f"{self.base_url}/api/s3cfg/{s3cfg_id}"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.delete(url, headers=self.headers)
+
+        if response.status_code == 204:
+            return
+        else:
+            raise Exception(f"Failed to delete s3cfg. Status code: {response.status_code}  Response: {response.content}")
+
+    def list_identifiers(self):
+        """
+        List all identifiers.
+        """
+        url = f"{self.base_url}/api/identifier/list"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to list identifiers. Status code: {response.status_code}  Response: {response.content}")
+
+    def get_identifier(self, identifier_name):
+        """
+        Get identifier by name.
+        """
+        url = f"{self.base_url}/api/identifier/{identifier_name}"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to get identifier (name {identifier_name}). Status code: {response.status_code}  Response: {response.content}")
+
+    def create_identifier(self, create_params: dict):
+        """
+        Create an identifier.
+        """
+        url = f"{self.base_url}/api/identifier"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.post(url, headers=self.headers, json=create_params)
+
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 422:
+            raise BadRequestException(f"Invalid request: {create_params}. Status code: {response.status_code}  Response: {response.content}")
+        else:
+            raise Exception(f"Failed to create identifier. Status code: {response.status_code}  Response: {response.content}")
+
+    def update_identifier(self, update_params: dict):
+        """
+        Update an identifier.
+        """
+        url = f"{self.base_url}/api/identifier"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.put(url, headers=self.headers, json=update_params)
+
+        if response.status_code == 204:
+            return
+        elif response.status_code == 422:
+            raise BadRequestException(f"Invalid request: {update_params}. Status code: {response.status_code}  Response: {response.content}")
+        else:
+            raise Exception(f"Failed to update identifier. Status code: {response.status_code}  Response: {response.content}")
+
+    def delete_identifier(self, delete_params: dict):
+        """
+        Delete an identifier.
+        """
+        url = f"{self.base_url}/api/identifier"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.delete(url, headers=self.headers, json=delete_params)
+
+        if response.status_code == 204:
+            return
+        else:
+            raise Exception(f"Failed to delete identifier. Status code: {response.status_code}  Response: {response.content}")
+
+    def upload_media(self, upload_params: dict):
+        """
+        upload a media object.
+        """
+        url = f"{self.base_url}/api/upload"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.post(url, headers=self.headers, json=upload_params)
+
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 422:
+            raise BadRequestException(f"Invalid request: {upload_params}. Status code: {response.status_code}  Response: {response.content}")
+        else:
+            raise Exception(f"Failed to create identifier. Status code: {response.status_code}  Response: {response.content}")
+
+    def get_download_media_urls(self, download_pids: list[str]):
+        """
+        Get download urls for multiple media objects.
+        """
+        url = f"{self.base_url}/api/download/urls"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.post(url, headers=self.headers, json=download_pids)
+
+        if response.status_code == 200:
+            return response.json()
+        elif response.status_code == 422:
+            raise BadRequestException(f"Invalid request: {download_pids}. Status code: {response.status_code}  Response: {response.content}")
+        else:
+            raise Exception(f"Failed to create identifier. Status code: {response.status_code}  Response: {response.content}")
+
+    def get_download_media_url(self, media_pid):
+        """
+        Get a download url for a media object.
+        """
+        url = f"{self.base_url}/api/download/url/{media_pid}"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to get download url for media object (name {media_pid}). Status code: {response.status_code}  Response: {response.content}")
+
+    def get_download_media(self, media_pid):
+        """
+        Directly download a media object.
+        """
+        url = f"{self.base_url}/api/download/{media_pid}"
+
+        if not self.token:
+            raise ClientError("No bearer token found. Please login first.")
+
+        response = requests.get(url, headers=self.headers)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            raise Exception(f"Failed to get media object (name {media_pid}). Status code: {response.status_code}  Response: {response.content}")
+
+    # def make_request(self, url, method, params=None):
+    #     if not self.token:
+    #         raise ClientError("No bearer token found. Please login first.")
+
+    #     if method == 'get':
+    #         response = requests.get(url, headers=self.headers)
+    #     elif method == 'post' or method == 'put':
+    #         response = requests.post(url, headers=self.headers, json=params)
+    #     elif method == 'delete':
+    #         response = requests.delete(url, headers=self.headers)
+    #     else:
+    #         raise Exception(f'Method not supported: {method}')
+
+    #     sc = response.status_code
+    #     if sc == 200:
+    #         return response.json()
+    #     elif sc == 204:
+    #         return
+    #     elif sc == 422:
+    #         raise BadRequestException(f"Invalid request: {params}. Status code: {sc}  Response: {response.content}")
+    #     else:
+    #         raise Exception(f"Failed to execute {method} request. Status code: {sc}, Response: {response.content}")
+
+    #     # Example usage
+    #     url = f"{self.base_url}/api/some_model/{model_id}"
+    #     return self.make_request(url, 'get')
--- test.py
+++ test.py
@@ -1,5 +1,9 @@
-# Usage Example
 from apiclient import ApiClient
+from utils.custom_exception import BadRequestException
+
+# Usage Example
+
+client = ApiClient(base_url="https://amplify-mediastore.whoi.edu", username="sa-mediastore", password="welcometo_homeofthe_")
 
 def test_media_create():
     # Initialize the client with your API base URL and credentials
@@ -27,5 +31,183 @@ def test_media_create():
     except Exception as e:
         print(str(e))
 
+
+def list_stores():
+    stores = client.list_stores()
+    print(f'All stores: {stores}')
+
+def test_create_store():
+    print('\nCreating store')
+    print('================')
+    # Create store
+    create_params = {
+        "type": "DictStore",
+        "bucket": "test_bucket",
+        "s3_url": ""
+    }
+    store = client.create_store(create_params)
+    print(f'Created store: {store}')
+    # Remove store
+    store_id = store['pk']
+    client.delete_store(store_id)
+    print(f'Removed store {store_id}')
+
+def test_create_invalid_store():
+    print('\nCreating store with invalid bucket type')
+    print('================')
+    # This should raise BadRequestException
+    create_params = {
+        "type": "InvalidStore", #invalid bucket type
+        "bucket": "empty_bucket",
+        "s3_url": ""
+    }
+    try:
+       client.create_store(create_params)
+    except BadRequestException as e:
+        print(f'BadRequestException: {str(e)}')
+
+def test_create_invalid_store_params():
+    print('\nCreating store with invalid params')
+    print('================')
+    # This should raise BadRequestException
+    create_params = {
+        "bingo": "bongo" #invalid parameters
+    }
+    try:
+       client.create_store(create_params)
+    except BadRequestException as e:
+        print(f'BadRequestException: {str(e)}')
+
+def test_create_invalid_bucket_store():
+    print('\nCreating invalid bucket store')
+    print('================')
+    # This should raise BadRequestException
+    create_params = {
+        "type": "BucketStore",
+        "bucket": "test_bucket",
+        "s3_url": "" #missing s3_url
+    }
+    try:
+       client.create_store(create_params)
+    except BadRequestException as e:
+        print(f'BadRequestException: {str(e)}')
+
+def list_s3cfgs():
+    s3cfgs = client.list_s3cfgs()
+    print(f'All s3cfgs: {s3cfgs}')
+
+def test_create_s3cfg():
+    print('\nCreating s3cfg')
+    print('================')
+    # Create s3cfg
+    create_params = {
+        "url": "",
+        "access_key": "",
+        "secret_key": ""
+    }
+    s3cfg = client.create_s3cfg(create_params)
+    print(f's3cfg successfully created: {s3cfg}')
+    # Remove s3cfg
+    s3cfg_id = s3cfg['pk']
+    client.delete_s3cfg(s3cfg_id)
+    print(f'Removed s3cfg {s3cfg_id}.')
+
+def test_create_invalid_s3cfg():
+    print('\nCreating invalid s3cfg')
+    print('================')
+    create_params = {
+        "bad_param": "breaking bad"
+    }
+    try:
+        s3cfg = client.create_s3cfg(create_params)
+        print(s3cfg)
+    except Exception as e:
+        print(str(e))
+
+def test_update_s3cfg():
+    print('\nUpdating s3cfg')
+    print('================')
+    # Create s3cfg
+    original_params = {
+        "url": "localhost:8000",
+        "access_key": "old1",
+        "secret_key": "old2"
+    }
+    s3cfg = client.create_s3cfg(original_params)
+    s3cfg_id = s3cfg['pk']
+    print(f'Original s3cfg: {s3cfg}')
+    # Update s3cfg
+    updated_params = {
+        "url": "localhost:8001",
+        "access_key": "new1",
+        "secret_key": "new2"
+    }
+    client.update_s3cfg(s3cfg_id, updated_params)
+    updated_s3cfg = client.get_s3cfg(s3cfg_id)
+    print(f'Updated s3cfg: {updated_s3cfg}')
+    # Remove s3cfg to avoid clogging
+    s3cfg_id = s3cfg['pk']
+    client.delete_s3cfg(s3cfg_id)
+    print(f'Removed s3cfg {s3cfg_id}')
+
+def list_identifiers():
+    identifiers = client.list_identifiers()
+    print(f'All identifiers: {identifiers}')
+
+# def test_create_identifier():
+#     print('\nCreating identifier')
+#     print('================')
+#     # Create identifier
+#     create_params = {
+#         "name": "demo_identifier",
+#         "pattern": "*foo*"
+#     }
+#     identifier = client.create_identifier(create_params)
+#     print(f'identifier successfully created: {identifier}')
+#     # Remove identifier
+#     client.delete_identifier(identifier)
+#     print(f'Removed identifier {identifier}.')
+
+# def test_create_invalid_identifier():
+#     print('\nCreating identifier with invalid params')
+#     print('================')
+#     # This should raise BadRequestException
+#     create_params = {
+#         "whoami": "foo bar" #invalid parameters
+#     }
+#     try:
+#        client.create_identifier(create_params)
+#     except BadRequestException as e:
+#         print(f'BadRequestException: {str(e)}')
+
+# def test_update_identifier():
+#     pass
+
+# def test_upload_download_media():
+#     pass
+
 if __name__ == "__main__":
-    test_media_create()
+    # test_media_create()
+
+    test_create_store()
+    test_create_invalid_store()
+    test_create_invalid_store_params()
+    test_create_invalid_bucket_store()
+
+    test_create_s3cfg()
+    test_create_invalid_s3cfg()
+    test_update_s3cfg()
+
+    # list_identifiers()
+    # test_create_identifier()
+    # list_identifiers()
+    # test_create_invalid_identifier()
+    # test_update_identifier()
+
+    # test_upload_download_media()
+
+    print('\nLists of objects in the mediastore')
+    print('================')
+    list_stores()
+    list_s3cfgs()
+    list_identifiers()
